dd5f9fa23b6f791227c09605902a91ce27f0aac7
Ugolin Ollé <ugolinolle.pro@gmail.com>
2022-12-08 09:45:51 -0500

    refactor: add texts to banner

--- components/banner.js
+++ components/banner.js
@@ -3,9 +3,9 @@ import Textra from 'react-textra';
 
 export const Banner = () => {
   const slideText = [
-    'Welcome to React Native Text Slider',
-    'It is very easy to use',
-    'You can simply install and use it now',
+    'Trouver la solution à vos projets.',
+    'Pimper votre site !',
+    'Customizer votre site de A à Z.'
   ];
 
   return (
@@ -15,7 +15,7 @@ export const Banner = () => {
         data={slideText}
         duration={200}
         stopduration={2000}
-        className="text-white lg:text-3xl text-[18px] px-2 font-poppins"
+        className="dark:text-white lg:text-5xl text-[18px] px-2 font-poppins"
       />
     </div>
   );
