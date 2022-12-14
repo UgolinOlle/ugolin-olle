d307e6384dd110e7fe7ff307d27a3d1aff4c9e23
Ugolin Ollé <ugolinolle.pro@gmail.com>
2022-12-13 22:28:26 -0500

    style: add z-index

--- components/contact-button.js
+++ components/contact-button.js
@@ -3,9 +3,8 @@ import { FiMail } from 'react-icons/fi';
 
 export const ContactButton = () => {
   return (
-    <div
-      className='opacity-70 hover:opacity-100 transition-all flex items-center justify-center text-[34px] w-[80px] h-[80px] rounded-full fixed bottom-5 right-3 bg-dark dark:bg-white text-white dark:text-dark'>
-      <a href='mailto:ugolin@olle.pro'>
+    <div className="z-10 opacity-70 hover:opacity-100 transition-all flex items-center justify-center text-[34px] w-[80px] h-[80px] rounded-full fixed bottom-5 right-3 bg-dark dark:bg-white text-white dark:text-dark">
+      <a href="mailto:ugolin@olle.pro">
         <FiMail />
       </a>
     </div>
